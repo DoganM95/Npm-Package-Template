f393d23bd352d67ce16295a8c406223fece3b27c
DoganM95 <doganmermertas@hotmail.de>
2026-01-17 23:25:29 +0100

    POC template

---
+++ index.js
@@ -0,0 +1,11 @@
+class ExampleClass {
+    constructor(userName) {
+        this.userName = userName;
+    }
+
+    getUserName() {
+        return this.userName;
+    }
+}
+
+module.exports = ExampleClass;
